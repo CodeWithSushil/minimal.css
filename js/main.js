de7b4c0f7df2a5e192802fffadb251962ea9457e
CodeWithSushil <sushilkumbhar77@gmail.com>
2023-08-17 20:35:09 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//alert('hi')
+const date = new Date();
+const year = date.getFullYear();
+//console.log(year)
+document.querySelector("#year").innerHTML = year;
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-3855518-1', 'auto');
+ga('require', 'displayfeatures');
+ga('require', 'GTM-WJ88MZ5');
+ga('send', 'pageview');
